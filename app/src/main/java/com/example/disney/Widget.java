220526569984405df92edc3d2fcef83ae05d12d7
Maria Eduarda Cavalcante <maria.nascimento343@etec.sp.gov.br>
2023-06-19 16:55:07 -0300

    Widget Completo

--- app/src/main/java/com/example/disney/Widget.java
+++ app/src/main/java/com/example/disney/Widget.java
@@ -19,7 +19,6 @@ public class Widget extends AppWidgetProvider  {
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
 
-
             Calendar calendar = Calendar.getInstance();
             int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
 
@@ -29,30 +28,55 @@ public class Widget extends AppWidgetProvider  {
                     Intent intent = new Intent(context, pesquisa.class);
                     String message = "Moana";
                     intent.putExtra(EXTRA_MESSAGE, message);
-                    PendingIntent pendingIntent = PendingIntent.getActivity(context,0, intent, 0);
+                    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
                     views.setOnClickPendingIntent(R.id.button, pendingIntent);
                     break;
                 case Calendar.TUESDAY:
-                    views.setImageViewResource(R.id.imgPersonaWidget, R.drawable.moana);
+                    views.setImageViewResource(R.id.imgPersonaWidget, R.drawable.bela);
+                    Intent intent2 = new Intent(context, pesquisa.class);
+                    String message2 = "Bela";
+                    intent2.putExtra(EXTRA_MESSAGE, message2);
+                    PendingIntent pendingIntent2 = PendingIntent.getActivity(context, 0, intent2, PendingIntent.FLAG_IMMUTABLE);
+                    views.setOnClickPendingIntent(R.id.button, pendingIntent2);
                     break;
                 case Calendar.WEDNESDAY:
-                    views.setImageViewResource(R.id.imgPersonaWidget, R.drawable.elsa);
+                    views.setImageViewResource(R.id.imgPersonaWidget, R.drawable.rapunzel);
+                    Intent intent3 = new Intent(context, pesquisa.class);
+                    String message3 = "Rapunzel";
+                    intent3.putExtra(EXTRA_MESSAGE, message3);
+                    PendingIntent pendingIntent3 = PendingIntent.getActivity(context, 0, intent3, PendingIntent.FLAG_IMMUTABLE);
+                    views.setOnClickPendingIntent(R.id.button, pendingIntent3);
                     break;
                 case Calendar.THURSDAY:
-                    views.setImageViewResource(R.id.imgPersonaWidget, R.drawable.moana);
+                    views.setImageViewResource(R.id.imgPersonaWidget, R.drawable.cinderela);
+                    Intent intent4 = new Intent(context, pesquisa.class);
+                    String message4 = "Cinderella";
+                    intent4.putExtra(EXTRA_MESSAGE, message4);
+                    PendingIntent pendingIntent4 = PendingIntent.getActivity(context, 0, intent4, PendingIntent.FLAG_IMMUTABLE);
+                    views.setOnClickPendingIntent(R.id.button, pendingIntent4);
                     break;
                 case Calendar.FRIDAY:
-                    views.setImageViewResource(R.id.imgPersonaWidget, R.drawable.elsa);
+                    views.setImageViewResource(R.id.imgPersonaWidget, R.drawable.tiana);
+                    Intent intent5 = new Intent(context, pesquisa.class);
+                    String message5 = "Tiana";
+                    intent5.putExtra(EXTRA_MESSAGE, message5);
+                    PendingIntent pendingIntent5 = PendingIntent.getActivity(context, 0, intent5, PendingIntent.FLAG_IMMUTABLE);
+                    views.setOnClickPendingIntent(R.id.button, pendingIntent5);
                     break;
                 case Calendar.SATURDAY:
-                    views.setImageViewResource(R.id.imgPersonaWidget, R.drawable.moana);
+                    views.setImageViewResource(R.id.imgPersonaWidget, R.drawable.jasmine);
+                    Intent intent6 = new Intent(context, pesquisa.class);
+                    String message6 = "Jasmine";
+                    intent6.putExtra(EXTRA_MESSAGE, message6);
+                    PendingIntent pendingIntent6 = PendingIntent.getActivity(context, 0, intent6, PendingIntent.FLAG_IMMUTABLE);
+                    views.setOnClickPendingIntent(R.id.button, pendingIntent6);
                     break;
                 case Calendar.SUNDAY:
                     views.setImageViewResource(R.id.imgPersonaWidget, R.drawable.elsa);
                     Intent intent7 = new Intent(context, pesquisa.class);
                     String message7 = "Elsa";
                     intent7.putExtra(EXTRA_MESSAGE, message7);
-                    PendingIntent pendingIntent7 = PendingIntent.getActivity(context,0, intent7, 0);
+                    PendingIntent pendingIntent7 = PendingIntent.getActivity(context, 0, intent7, PendingIntent.FLAG_IMMUTABLE);
                     views.setOnClickPendingIntent(R.id.button, pendingIntent7);
                     break;
                 default:
